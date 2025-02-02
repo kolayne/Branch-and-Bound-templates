731efb320290d3ef69d92b23eff5bb030c939d2b
Nikolai Nechaev <nikolay_nechaev@mail.ru>
2025-02-02 14:58:08 +0300

    Implement the first version of the library
    Implemented the PoC of a library. The code is currently poorly
    structured and only supports best-first-search. The API is probably not
    so good yet either.
    
    Additionally implemented a (very artificial) example that walks a graph using
    the library solver.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,115 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::binary_heap::BinaryHeap;
+
+/// Represents the set of subproblems of an intermediate problem
+/// or the value of the objective function of a feasible solution (leaf node).
+pub enum SubproblemsOrScore<Node, Score: Ord> {
+    /// Subproblems of an intermediate problem
+    Subproblems(Box<dyn Iterator<Item = Node>>),
+    /// The value of the objective function of a feasible solution
+    Score(Score),
+}
+// TODO: Consider an alternative implementation by making the iterator
+// type a generic variable rather than a `dyn`
+
+use SubproblemsOrScore::{Score, Subproblems};
+
+/// Represents a problem that is to be solved with branch-and-bound
+pub trait Problem<Node, Score: Ord> {
+    /// The initial problem state
+    fn initial(&self) -> Node;
+
+    /// Processes a node.
+    ///
+    /// If the given node is a fesible solution, returns the value of the
+    /// objective function at it.
+    ///
+    /// If the given node is a subproblem that is to be split further
+    /// into subproblems, returns the set of its subproblems.
+    fn branch_or_evaluate(&self, node: &Node) -> SubproblemsOrScore<Node, Score>;
+
+    fn bound(&self, node: &Node) -> Score;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Candidate<Node, Score: Ord> {
+    node: Node,
+    /// Score is always defined.
+    /// For intermediate subproblems, it is the value of the bounding function.
+    /// When a node is discovered to be a leaf node, its score is to be replaced
+    /// with the value of the objective function.
+    score: Score,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Wraps a `Candidate` and implements `{Partial,}Eq` and `{Partial,}Ord`
+/// based on the score, ignoring the candidate.
+struct CandidateAsScore<Node, Score: Ord>(Candidate<Node, Score>);
+
+impl<Node, Score: Ord> PartialEq for CandidateAsScore<Node, Score> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.score == other.0.score
     }
+}
+
+impl<Node, Score: Ord> Eq for CandidateAsScore<Node, Score> {}
+
+impl<Node, Score: Ord> PartialOrd for CandidateAsScore<Node, Score> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.0.score.cmp(&other.0.score))
+    }
+}
+
+impl<Node, Score: Ord> Ord for CandidateAsScore<Node, Score> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+pub fn solve<Node, Score: Ord>(problem: &impl Problem<Node, Score>) -> Option<Node> {
+    let mut ans: Option<Candidate<Node, Score>> = None;
+
+    let mut queue = BinaryHeap::new();
+    queue.push(CandidateAsScore(Candidate {
+        node: problem.initial(),
+        score: problem.bound(&problem.initial()),
+    }));
+
+    while let Some(CandidateAsScore(candidate)) = queue.pop() {
+        if let Some(incumbent) = &ans {
+            if candidate.score < incumbent.score {
+                // When a candidate's _bound_ is worse than the incumbent's
+                // objective score, we don't need to search any further.
+                break;
+                // TODO: we can only break as easily in the BeFS case
+            }
+        }
+
+        match problem.branch_or_evaluate(&candidate.node) {
+            // Intermediate subproblem
+            Subproblems(subproblems) => {
+                for node in subproblems {
+                    let score = problem.bound(&node);
+                    queue.push(CandidateAsScore(Candidate { node, score }));
+                }
+            }
+
+            // Leaf node
+            Score(objective_score) => {
+                ans = match ans {
+                    None => Some(candidate),
+                    Some(incumbent) => {
+                        if incumbent.score < objective_score {
+                            // Replace the old (boundary) score with the objective score
+                            Some(Candidate {
+                                score: objective_score,
+                                ..candidate
+                            })
+                        } else {
+                            Some(incumbent)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    ans.and_then(|candidate| Some(candidate.node))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,90 @@
+use bb::SubproblemsOrScore::{Score, Subproblems};
 use branch_and_bound as bb;
 
+#[derive(Clone, Debug)]
+struct GraphNode {
+    bound: i32,
+    next: Vec<GraphNode>,
+}
+
+struct Problem {
+    root: GraphNode,
+}
+
+impl bb::Problem<GraphNode, i32> for Problem {
+    fn initial(&self) -> GraphNode {
+        self.root.clone()
+    }
+
+    fn branch_or_evaluate(
+        &self,
+        node: &GraphNode,
+    ) -> branch_and_bound::SubproblemsOrScore<GraphNode, i32> {
+        if node.bound < 5 {
+            eprintln!("I should not be visited in Best-First-Search");
+        } else {
+            eprintln!("Node with bound {0} visited", node.bound)
+        }
+        if node.next.is_empty() {
+            Score(node.bound)
+        } else {
+            Subproblems(Box::new(node.next.clone().into_iter()))
+        }
+    }
+
+    fn bound(&self, node: &GraphNode) -> i32 {
+        node.bound
+    }
+}
+
 fn main() {
-    println!("I can access the library code: {}", bb::add(123, 11));
+    let leaf0 = GraphNode {
+        bound: 0,
+        next: vec![],
+    };
+    let leaf1 = GraphNode {
+        bound: 1,
+        next: vec![],
+    };
+    let leaf2 = GraphNode {
+        bound: 2,
+        next: vec![],
+    };
+    let leaf3 = GraphNode {
+        bound: 3,
+        next: vec![],
+    };
+    let leaf4 = GraphNode {
+        bound: 4,
+        next: vec![],
+    };
+    let leaf5 = GraphNode {
+        bound: 5,
+        next: vec![],
+    };
+
+    let parent23 = GraphNode {
+        bound: 4,
+        next: vec![leaf2, leaf3],
+    };
+    let parent1p23 = GraphNode {
+        bound: 5,
+        next: vec![leaf1, parent23],
+    };
+
+    let parent45 = GraphNode {
+        bound: 6,
+        next: vec![leaf4, leaf5],
+    };
+    let parent0p45 = GraphNode {
+        bound: 7,
+        next: vec![leaf0, parent45],
+    };
+
+    let root = GraphNode {
+        bound: 8,
+        next: vec![parent1p23, parent0p45],
+    };
+
+    println!("Max node: {:#?}", bb::solve(&Problem { root }));
 }
