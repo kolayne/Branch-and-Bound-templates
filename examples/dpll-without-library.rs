16a4e61ecca44f547bfd9aac9108d02749fea0ea
Nikolai Nechaev <nikolay_nechaev@mail.ru>
2025-03-14 17:06:36 +0300

    Dpll example: improve performance by not repeating checks

--- examples/dpll-without-library.rs
+++ examples/dpll-without-library.rs
@@ -11,7 +11,6 @@ use dpll_common::*;
 // time by checking them again and again for future variables.
 
 fn solve_dfs(problem: &CnfSat, mut vars_left: &[u32], assignments: &mut [i8]) -> bool {
-    let mut recheck = true;
     let mut unknown_count = 0;
 
     let mut to_reset: Vec<usize> = vec![];
@@ -21,35 +20,39 @@ fn solve_dfs(problem: &CnfSat, mut vars_left: &[u32], assignments: &mut [i8]) ->
         }
     };
 
-    // It may be more efficient to only run this check once rather than in a loop.
-    // Although sometimes we may miss an eager variable assignment that could become
-    // possible after another eager variable assignment, we save time on repeated checks.
-    // TODO: check.
-    while recheck {
-        recheck = false;
-        unknown_count = 0;
-        // Validate the current assignments and try to eagerly assign some variables
-        for clause in &problem.clauses {
-            match clause.eval(assignments) {
-                ClauseState::Known(false) => {
-                    do_reset(&to_reset, assignments);
-                    return false;
+    // Validate the current assignments and try to eagerly assign some variables.
+    // Note that after eagerly assigning a variable, the following peculiar things may happen:
+    //
+    // 1. Another variable can now be eagerly assigned. If that follows from an earlier caluse,
+    //    we might miss that. But it's fine, as it will be checked in the following call anyway.
+    //
+    // 2. Another clause (which also only depended on the same variable) may become false
+    //    (invalidating the whole formula). If that clause was an earlier one, we miss that.
+    //
+    // We could overcome both of these issues by repeating this block of checks in case a variable
+    // was set eagerly. However, as tested in practice, this will worsen the performance.
+    //
+    // 2 could also be fixed by not applying eager assignments directly (first collect them and
+    // ensure there is no conflict, then apply), but I doubt there will be a notable improvement.
+    for clause in &problem.clauses {
+        match clause.eval(assignments) {
+            ClauseState::Known(false) => {
+                do_reset(&to_reset, assignments);
+                return false;
+            }
+            ClauseState::Known(true) => {}
+            ClauseState::OneLeft(literal) => {
+                // Assign a variable eagerly. May happen multiple times for the same variable
+                if literal < 0 {
+                    assignments[-literal as usize] = -1;
+                    to_reset.push(-literal as usize);
+                } else {
+                    assignments[literal as usize] = 1;
+                    to_reset.push(literal as usize);
                 }
-                ClauseState::Known(true) => {}
-                ClauseState::OneLeft(literal) => {
-                    // Assign a variable eagerly. May happen multiple times for the same variable
-                    if literal < 0 {
-                        assignments[-literal as usize] = -1;
-                        to_reset.push(-literal as usize);
-                    } else {
-                        assignments[literal as usize] = 1;
-                        to_reset.push(literal as usize);
-                    }
-                    recheck = true;
-                }
-                ClauseState::Unknown => {
-                    unknown_count += 1;
-                }
+            }
+            ClauseState::Unknown => {
+                unknown_count += 1;
             }
         }
     }
@@ -82,10 +85,11 @@ fn solve_dfs(problem: &CnfSat, mut vars_left: &[u32], assignments: &mut [i8]) ->
         return false;
     }
 
-    unreachable!(
-        "According to the initial validation, the formula is not yet decided,
-         but ran out of variables to check"
-    );
+    // The initial validation did not detect that the formula is decided,
+    // but we ran out of variables to check. This only happens if we've
+    // managed to eagerly assign the last variable. So, just perform
+    // the final validation!
+    solve_dfs(problem, vars_left, assignments)
 }
 
 fn solve(problem: &CnfSat) -> Option<Vec<i8>> {
