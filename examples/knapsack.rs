1ade92dccba2a3d86ae74b24a5509b3e3b78af71
Nikolai Nechaev <nikolay_nechaev@mail.ru>
2025-03-27 23:47:28 +0300

    Knapsack example: take out knapsack_common.rs
    Extract common structures and implementation to knapsack_common.rs;
    improve encapsulation and clean things up a bit.

--- examples/knapsack.rs
+++ examples/knapsack.rs
@@ -1,87 +1,42 @@
 use branch_and_bound::{Subproblem, SubproblemResolution};
 
-#[derive(Clone, Debug)]
-struct Item {
-    price: u32,
-    weight: u32,
-}
-
-#[derive(Default, Clone)]
-struct Knapsack {
-    /// Value currently acquired by the knapsack
-    val: u32,
-    /// Capacity left in the knapsack
-    capacity: u32,
-    /// Items to try to put (sorted by the `.price/.weight` ratio in ascending order)
-    items_left: Vec<Item>,
-    /// Items in the knapsack (only needed to restore the answer)
-    items_in: Vec<Item>,
-}
-
-impl Knapsack {
-    pub fn drop_too_heavy(&mut self) {
-        while let Some(item) = self.items_left.last() {
-            if item.weight > self.capacity {
-                self.items_left.pop();
-            } else {
-                break;
-            }
-        }
-    }
-
-    pub fn drop_last(&mut self) {
-        debug_assert!(!self.items_left.is_empty());
-        self.items_left.pop();
-    }
-
-    pub fn include_last(&mut self) {
-        debug_assert!(!self.items_left.is_empty());
-        let item = self.items_left.last().unwrap();
-        self.val += item.price;
-        self.capacity -= item.weight;
-        self.items_in.push(self.items_left.pop().unwrap());
-    }
-
-    pub fn have_items(&self) -> bool {
-        !self.items_left.is_empty()
-    }
-}
+mod knapsack_common;
+use knapsack_common::*;
 
 impl Subproblem for Knapsack {
     type Score = u32;
 
     fn branch_or_evaluate(&mut self) -> SubproblemResolution<Self, Self::Score> {
-        if self.capacity == 0 {
-            return SubproblemResolution::Solved(self.val);
+        if self.capacity_left() == 0 {
+            return SubproblemResolution::Solved(self.collected_val());
         }
-
-        self.drop_too_heavy();
 
         if self.have_items() {
             let mut child_include = self.clone();
-            child_include.include_last();
+            child_include.include_next();
 
-            let mut child_exclude: Knapsack = Default::default();
-            std::mem::swap(self, &mut child_exclude); // Avoid copying: swap with `self` instead
-            child_exclude.drop_last();
+            let dummy = Knapsack::new(0, vec![]);
+            let mut child_exclude = std::mem::replace(self, dummy); // Avoid copying: reuse `self`
+            child_exclude.drop_next();
 
             SubproblemResolution::Branched(Box::new([child_include, child_exclude].into_iter()))
         } else {
-            SubproblemResolution::Solved(self.val)
+            SubproblemResolution::Solved(self.collected_val())
         }
     }
 
     fn bound(&self) -> Self::Score {
-        // If I add items greadily and arrive exactly at the zero capacity,
-        // that's the best solution.
+        // If I was to add items greadily (items are ordered by ratio) and perfectly fill
+        // the knapsack, that would be the best solution.
+        //
         // The heuristic is as follows: I try to use a bit more than
         // the capacity of the knapsack and when that is filled, I claim that
         // that's the best we could possibly get
-        // (because that's the best we could possibly get with a larger knapsack).
+        // (because that's the best we could possibly get with a slightly larger knapsack).
 
-        let mut val = self.val;
-        let mut capacity = self.capacity;
-        for item in self.items_left.iter().rev() {
+        let mut val = self.collected_val();
+        let mut capacity = self.capacity_left();
+        for item in self.future_items() {
             if item.weight < capacity {
                 val += item.price;
                 capacity -= item.weight;
---
+++ examples/knapsack_common.rs
@@ -0,0 +1,90 @@
+#[derive(Clone, Debug)]
+pub struct Item {
+    pub price: u32,
+    pub weight: u32,
+}
+
+#[derive(Clone)]
+pub struct Knapsack {
+    /// Value currently acquired by the knapsack
+    val: u32,
+    /// Capacity left in the knapsack
+    capacity_left: u32,
+    /// Items to try to put (sorted by the `.price/.weight` ratio in ascending order).
+    ///
+    /// Internal invariant: if not empty, the last item's weight must be less than capacity
+    /// (otherwise gets popped). That is to make sure that all methods (`.drop_next()`,
+    /// `.include_next()`, `.future_items()`, `.have_items()`) are consistent.
+    items_left: Vec<Item>,
+    /// Items in the knapsack (only needed to restore the answer)
+    items_in: Vec<Item>,
+}
+
+impl Knapsack {
+    pub fn new(capacity: u32, items: Vec<Item>) -> Self {
+        let mut res = Self {
+            val: 0,
+            capacity_left: capacity,
+            items_left: items,
+            items_in: vec![],
+        };
+        // Sort by ratio in _ascending_ order (last is best)
+        res.items_left.sort_by(|item1, item2| {
+            (item1.price * item2.weight).cmp(&(item2.price * item1.weight))
+        });
+        res.pop_too_heavy();
+        res
+    }
+
+    fn pop_too_heavy(&mut self) {
+        while let Some(item) = self.items_left.last() {
+            if item.weight > self.capacity_left {
+                self.items_left.pop();
+            } else {
+                break;
+            }
+        }
+    }
+
+    pub fn drop_next(&mut self) {
+        debug_assert!(!self.items_left.is_empty());
+        self.items_left.pop();
+        self.pop_too_heavy();
+    }
+
+    pub fn include_next(&mut self) {
+        debug_assert!(!self.items_left.is_empty());
+        let item = self.items_left.last().unwrap();
+        self.val += item.price;
+        self.capacity_left -= item.weight;
+        self.items_in.push(self.items_left.pop().unwrap());
+        self.pop_too_heavy();
+    }
+
+    pub fn have_items(&self) -> bool {
+        !self.items_left.is_empty()
+    }
+
+    pub fn capacity_left(&self) -> u32 {
+        self.capacity_left
+    }
+
+    pub fn collected_val(&self) -> u32 {
+        self.val
+    }
+
+    /// Future items to be included, in the order they may be included
+    /// (i.e., descending order of the `.price / .weight` ratio).
+    /// Elements with weight exceeding capacity are excluded.
+    pub fn future_items(&self) -> impl Iterator<Item = &Item> {
+        self.items_left
+            .iter()
+            .rev()
+            .filter(|item| item.weight <= self.capacity_left)
+    }
+
+    /// Converts a `Knapsack` into the set of items that are in the knapsack.
+    pub fn into_items(self) -> Vec<Item> {
+        self.items_in
+    }
+}
